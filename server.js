2c0692d4dbb0b1141f6b2f398ee3f6638c71e64f
jcao91 <jcao91@yahoo.com>
2024-06-04 10:40:38 -0500

    created new route

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-const express = require("express");
-const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
+const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
 
 mongoose.connect(process.env.MONGODB_URI);
@@ -14,6 +15,26 @@ const Food = require("./models/food.js");
 app.get("/", async (req, res) => {
   res.render("index.ejs");
 });
+
+app.get("/foods/new", (req, res) => {
+  res.render("foods/new.ejs");
+});
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
