1c68aa58a7c174939e276251a9408ede815fb835
jcao91 <jcao91@yahoo.com>
2024-06-06 10:54:29 -0500

    edit function finished and css ink created

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
 const mongoose = require("mongoose");
-const methodOverride = require('method-override');
-const morgan = require('morgan');
+const methodOverride = require("method-override");
+const morgan = require("morgan");
+const path = require("path");
+const port = process.env.PORT || "3000";
 
 const app = express();
 
 // Set the view engine to EJS
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 mongoose.connect(process.env.MONGODB_URI);
 mongoose.connection.on("connected", () => {
@@ -19,8 +21,9 @@ const Food = require("./models/food.js");
 
 app.use(express.urlencoded({ extended: false }));
 // this was created after making the new food submit form and before defining the post route
-app.use(methodOverride('_method'));
-app.use(morgan('dev'));
+app.use(methodOverride("_method"));
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", async (req, res) => {
   res.render("index.ejs");
@@ -52,17 +55,25 @@ app.post("/foods", async (req, res) => {
 });
 
 app.get("/foods/:foodId/edit", async (req, res) => {
-    const foundFood = await Food.findById(req.params.foodId);
-    res.render("foods/edit.ejs", { food: foundFood, });
-  });
-  
-
-
-app.delete('/foods/:foodId', async (req, res) =>{
-    await Food.findByIdAndDelete(req.params.foodId);
-    res.redirect('/foods');
+  const foundFood = await Food.findById(req.params.foodId);
+  res.render("foods/edit.ejs", { food: foundFood });
 });
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
+app.put("/foods/:foodId", async (req, res) => {
+  if (req.body.isHealthy === "on") {
+    req.body.isHealthy = true;
+  } else {
+    req.body.isHealthy = false;
+  }
+  await Food.findByIdAndUpdate(req.params.foodId, req.body);
+  res.redirect(`/foods/${req.params.foodId}`);
+});
+
+app.delete("/foods/:foodId", async (req, res) => {
+  await Food.findByIdAndDelete(req.params.foodId);
+  res.redirect("/foods");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
